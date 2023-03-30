5467d16a0d55dd16ebb84ddf15a18555146f4311
abhinav <singh.abhinav6996@gmail.com>
2023-03-30 16:30:49 +0530

    chore: add pagination on fetched repos

--- src/components/RepoCard.jsx
+++ src/components/RepoCard.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
-export const RepoCard = () => {
+export const RepoCard = ({ data }) => {
+  console.log(data);
   return (
-    <div>
-      RepoCard
+    <div className="repoCard">
+      <h3>
+        <a href={data.html_url} target="_blank" rel="noreferrer">
+          {data.name}
+        </a>
+      </h3>
+      <p>{data.description}</p>
+      <ul>
+        <li>Stars: {data.stargazers_count}</li>
+        <li>Watchers: {data.watchers_count}</li>
+        <li>Forks: {data.forks_count}</li>
+      </ul>
     </div>
-  )
-}
+  );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,46 +1,72 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Header } from "../components/Header";
 import { SortDropdown } from "../components/SortDropdown";
 import { TextField } from "../components/TextField";
 import "../stylesheets/home.css";
+import { RepoCard } from "../components/RepoCard";
+
+const PAGE_SIZE = 6;
 
 export const Home = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [repos, setRepos] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
 
-  async function fetchRepos() {
-    const url = `https://api.github.com/search/repositories?q=${searchTerm}`;
-    const headers = {
-      Authorization: process.env.ACCESS_TOKEN,
+  useEffect(() => {
+    const fetchRepos = async () => {
+      if (!searchTerm) {
+        return;
+      }
+
+      const url = `https://api.github.com/search/repositories?q=${searchTerm}&sort=stars&order=desc&page=${currentPage}&per_page=6`;
+      const headers = {
+        Authorization: process.env.ACCESS_TOKEN,
+      };
+      try {
+        const response = await axios.get(url, { headers });
+        setRepos((prevRepos) => [...prevRepos, ...response.data.items]);
+      } catch (error) {
+        console.error(error);
+      }
     };
 
-    try {
-      const response = await axios.get(url, { headers });
-      const repos = response.data.items;
-      setRepos(repos);
-    } catch (error) {
-      console.error(error);
-    }
-  }
+    fetchRepos();
+  }, [searchTerm, currentPage]);
 
-  function handleSearch() {
-    fetchRepos(searchTerm);
-  }
+  const handleSearch = () => {
+    setCurrentPage(1);
+  };
 
   function handleInputChange(event) {
     setSearchTerm(event.target.value);
   }
 
-  function handleKeyUp(event) {
-    console.log(event)
+  const handleKeyUp = (event) => {
     if (event.key === "Enter") {
-      event.preventDefault();
       handleSearch();
     }
-  }
+  };
 
-  console.log(searchTerm)
+  const handlePaginationClick = (event) => {
+    setCurrentPage(Number(event.target.id));
+  };
+
+  const handlePreviousClick = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNextClick = () => {
+    if (repos.length === PAGE_SIZE) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const indexOfLastRepo = currentPage * PAGE_SIZE;
+  const indexOfFirstRepo = indexOfLastRepo - PAGE_SIZE;
+  const currentRepos = repos.slice(indexOfFirstRepo, indexOfLastRepo);
 
   return (
     <>
@@ -53,6 +79,30 @@ export const Home = () => {
         />
         <SortDropdown />
       </section>
+      <section>
+        {currentRepos.map((repo, index) => (
+          <RepoCard key={index} data={repo} />
+        ))}
+      </section>
+      <div>
+        <button onClick={handlePreviousClick}>Previous</button>
+        {repos.length > 0 &&
+          Array(Math.ceil(repos.length / PAGE_SIZE))
+            .fill()
+            .map((_, index) => (
+              <button
+                key={index}
+                id={index + 1}
+                onClick={handlePaginationClick}
+                disabled={currentPage === index + 1}
+              >
+                {index + 1}
+              </button>
+            ))}
+        <button onClick={handleNextClick} disabled={repos.length !== PAGE_SIZE}>
+          Next
+        </button>
+      </div>
     </>
   );
 };
