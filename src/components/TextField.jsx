765ade36398b5ed6392560233ac4f939b6a1ecb6
abhinav <singh.abhinav6996@gmail.com>
2023-03-30 14:29:49 +0530

    chore: add input handling and repo fetch

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../stylesheets/textfield.css'
 
-export const TextField = ({ searchTerm, handleInputChange, handleKeyDown }) => {
+export const TextField = ({ searchTerm, handleInputChange, handleKeyUp }) => {
   return (
     <>
       <div className="fullWidth">
@@ -11,7 +11,7 @@ export const TextField = ({ searchTerm, handleInputChange, handleKeyDown }) => {
           placeholder="Search any repo..."
           value={searchTerm}
           onChange={handleInputChange}
-          onKeyDown={handleKeyDown}
+          onKeyUp={handleKeyUp}
         />
       </div>
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,58 @@
-import React, {useState} from 'react';
-import axios from 'axios';
-import { Header } from '../components/Header';
-import { SortDropdown } from '../components/SortDropdown';
-import { TextField } from '../components/TextField';
-import '../stylesheets/home.css';
+import React, { useState } from "react";
+import axios from "axios";
+import { Header } from "../components/Header";
+import { SortDropdown } from "../components/SortDropdown";
+import { TextField } from "../components/TextField";
+import "../stylesheets/home.css";
 
 export const Home = () => {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
+  const [repos, setRepos] = useState([]);
+
+  async function fetchRepos() {
+    const url = `https://api.github.com/search/repositories?q=${searchTerm}`;
+    const headers = {
+      Authorization: process.env.ACCESS_TOKEN,
+    };
+
+    try {
+      const response = await axios.get(url, { headers });
+      const repos = response.data.items;
+      setRepos(repos);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  function handleSearch() {
+    fetchRepos(searchTerm);
+  }
 
   function handleInputChange(event) {
     setSearchTerm(event.target.value);
   }
 
-  function handleKeyDown(event) {
+  function handleKeyUp(event) {
+    console.log(event)
     if (event.key === "Enter") {
       event.preventDefault();
+      handleSearch();
     }
   }
 
+  console.log(searchTerm)
 
   return (
     <>
-      <Header/>
-      <section className='inputWrapper'>
-        <TextField searchTerm={searchTerm} handleInputChange={handleInputChange} onKeyDown={handleKeyDown} />
-        <SortDropdown/>
+      <Header />
+      <section className="inputWrapper">
+        <TextField
+          searchTerm={searchTerm}
+          handleInputChange={handleInputChange}
+          handleKeyUp={handleKeyUp}
+        />
+        <SortDropdown />
       </section>
     </>
   );
-}
+};
